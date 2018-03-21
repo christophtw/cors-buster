45140b68c5f96b40c148c354603a938f328a8c4d
Christoph Twesten <christoph.twesten@perfood.de>
2018-03-21 23:06:10 +0100

    Installed ionic-native-http-connection-backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpBackend, HttpXhrBackend } from '@angular/common/http';
+import { NativeHttpModule, NativeHttpBackend, NativeHttpFallback } from 'ionic-native-http-connection-backend';
+import { Platform } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,6 +19,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    NativeHttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,6 +31,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    {provide: HttpBackend, useClass: NativeHttpFallback, deps: [Platform, NativeHttpBackend, HttpXhrBackend]},
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Platform } from 'ionic-angular';
+
+import { HTTP } from '@ionic-native/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+string: string = "";
+cordova: string = "";
+
+  constructor(  public navCtrl: NavController,
+                private platform: Platform,
+                private nativeHttp: HTTP,
+                private http: HttpClient  ) {
 
   }
+
+ionViewDidLoad() {
+  this.http.get('https://www.dropbox.com', {responseType: "arraybuffer"})
+  .subscribe(res => {
+    console.log(res)
+    this.string = "GET Ok"
+  }, (err) => {
+    console.log(err)
+    this.string = "GET Failed"
+
+  })
+
+  this.platform.ready().then(() => {
+       this.nativeHttp.post('https://httpbin.org/post', {a: 'b'}, {})
+           .then(() => {
+               console.log('cordova-plugin-advanced-http is installed properly');
+               this.cordova = "cordova-plugin-advanced-http is installed properly"
+           });
+   });
+
+}
 
 }
